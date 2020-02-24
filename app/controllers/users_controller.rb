a31f559c2ddd254f5394398477e21af2566515ef
goetaro <goemon.fever556@gmail.com>
2020-02-24 08:37:07 +0000

    user signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      flash[:notice] = "ユーザー登録に成功しました"
+      redirect_to root_path
+    else
+      flash[:notice] = "ユーザー登録に失敗しました"
+      render :new
+    end
+  end
+end
+
+
+private
+
+def user_params
+  params.require(:user).permit(:name, :email, :password, :password_confirmation)
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
   get 'toppages/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "toppages#index"
+  
+  get "signup", to: "users#new"
+  resources :users, only: [:new, :create]
   
   resources :tasks
 end
