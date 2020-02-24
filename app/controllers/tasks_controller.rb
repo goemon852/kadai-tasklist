209d163ea8aec39c0fef0ebda015a8496476950c
goetaro <goemon.fever556@gmail.com>
2020-02-24 14:32:00 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,18 +1,22 @@
 class TasksController < ApplicationController
+    
+    before_action :require_user_logged_in
+    before_action :correct_user, only: [:destroy]
+    
     def index
-        @tasks = Task.all
+        @tasks = current_user.tasks.order(id: :desc)
     end
     
     def show
-        @task = Task.find(params[:id])
+        @task = Task.find(params[:id])  
     end
     
     def new
-        @task = Task.new
+        @task = current_user.tasks.build  # form_with 用
     end
     
     def create
-        @task = Task.new(task_params)
+        @task = current_user.tasks.build(task_params)
         
         if @task.save
             flash[:notice] = "タスクが正常に追加されました"
@@ -40,11 +44,9 @@ class TasksController < ApplicationController
     end
     
     def destroy
-        @task = Task.find(params[:id])
         @task.destroy
-        
-        flash[:notice] = "タスクが正常に削除されました"
-        redirect_to tasks_url
+        flash[:notice] = 'タスクが正常に削除されました'
+        redirect_to root_path
     end
     
     private
@@ -53,4 +55,12 @@ class TasksController < ApplicationController
     def task_params
         params.require(:task).permit(:content, :status) 
     end
+    
+    def correct_user
+        @task = current_user.tasks.find_by(id: params[:id])
+        unless @task
+            redirect_to root_url
+        end
+    end    
+    
 end
